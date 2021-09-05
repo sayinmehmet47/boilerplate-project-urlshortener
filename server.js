85f5dd16b0e715d8b084ade337f6bee5135616b9
sayin47 <sayinmehmet47@gmail.com>
2021-09-05 14:19:17 +0300

    url edited

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const { urlencoded } = require('body-parser');
 const app = express();
-
-// Basic Configuration
 const port = process.env.PORT || 3000;
+const shorten = require('simple-short');
 
 app.use(cors());
+app.use(express.json());
+app.use(urlencoded({ extended: false }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+
+app.post('/api/shorturl', (req, res) => {
+  var sid = shorten(req.body.url);
+  console.log(sid);
+  res.json({
+    original_url: req.body.url,
+    short_url: sid,
+  });
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
